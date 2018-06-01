30df4d0c0b40d6b4270a7371b02727e29b30f27f
Glory Lo <glo@pof.com>
2018-06-01 12:03:48 -0700

    fixed tests

--- FuzzyWuzzy.Tests/StringDiffTests.cs
+++ FuzzyWuzzy.Tests/StringDiffTests.cs
@@ -21,38 +21,46 @@ namespace FuzzyWuzzy.Tests
             Assert.Equal(expectedDistance, distance);
         }
 
-//        public void GetEditDistanceTrivalCaseOneLeftString()
-//        {
-//            var distance = StringDiff.Distance("cars", string.Empty);
-//            Assert.That(distance == 4);
-//        }
-//
-//        [Test]
-//        public void GetEditDistanceDeleteCase()
-//        {
-//            var distance = StringDiff.Distance("cars", "carts");
-//            Assert.That(distance == 1);
-//        }
-//
-//        [Test]
-//        public void GetEditDistanceSubstitutionCase()
-//        {
-//            var distance = StringDiff.Distance("cars", "cats");
-//            Assert.That(distance == 1);
-//        }
-//
-//        [Test]
-//        public void GetEditDistanceInsertCase()
-//        {
-//            var distance = StringDiff.Distance("car", "cars");
-//            Assert.That(distance == 1);
-//        }
-//
-//        [Test]
-//        public void GetEditDistanceComplexCase()
-//        {
-//            var distance = StringDiff.Distance("cardudmum", "cardamom");
-//            Assert.That(distance == 3);
-//        }
+        [Theory]
+        [InlineData("cars", "", 4)]
+
+        public void GetEditDistanceTrivalCaseOneLeftString(string text, string compareText, int expectedDistance)
+        {
+            var distance = StringDiff.Distance(text, compareText);
+            Assert.True(distance == expectedDistance);
+        }
+
+        [Theory]
+        [InlineData("cars", "carts", 1)]
+
+        public void GetEditDistanceDeleteCase(string text, string compareText, int expectedDistance)
+        {
+            var distance = StringDiff.Distance(text, compareText);
+            Assert.True(distance == expectedDistance);
+        }
+
+        [Theory]
+        [InlineData("cars", "cats", 1)]
+        public void GetEditDistanceSubstitutionCase(string text, string compareText, int expectedDistance)
+        {
+            var distance = StringDiff.Distance(text, compareText);
+            Assert.True(distance == expectedDistance);
+        }
+
+        [Theory]
+        [InlineData("car", "cars", 1)]
+        public void GetEditDistanceInsertCase(string text, string compareText, int expectedDistance)
+        {
+            var distance = StringDiff.Distance(text, compareText);
+            Assert.True(distance == expectedDistance);
+        }
+        [Theory]
+        [InlineData("cardudmum", "cardamom", 3)]
+        [InlineData("synergy", "energy", 2)]
+        public void GetEditDistanceComplexCase(string text, string compareText, int expectedDistance)
+        {
+            var distance = StringDiff.Distance(text, compareText);
+            Assert.True(distance == expectedDistance);
+        }
     }
 }
